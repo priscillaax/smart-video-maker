d5efc204f3402d1a31ac02143d0b15051e230127
priscilla.nascimento <priscilla.nascimento@esig.com.br>
2019-09-29 14:56:53 -0300

    iniciando o robo de texto

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const readline = require('readline-sync')
+const robots = {
+    //input: require('./robots/input.js'),
+    text: require('./robots/text.js')
+}
 
-function start() {
+async function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefix()
+
+    //robots.input(content)
+    await robots.text(content)
 
     function askAndReturnSearchTerm() {
         return readline.question('Type a Wikipedia search term: ')
